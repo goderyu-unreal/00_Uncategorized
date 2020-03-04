4dd3a0715f31088f062163a7f16825da4ce56324
goderyu <goderyu@gmail.com>
2020-03-04 14:57:37 +0800

    非正常库，利用ActorSequence来动态加载释放的小框架

---
+++ Uncategorized/Plugins/AbnormalPlugin/Source/AbnormalPlugin/Private/AbnormalManager.cpp
@@ -0,0 +1,124 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbnormalManager.h"
+#include "EngineUtils.h"
+#include "Engine/EngineTypes.h"
+#include "Engine/TargetPoint.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+
+#define OUT
+
+DEFINE_LOG_CATEGORY_STATIC(LogAbnormalPlugin, Log, All);
+// Sets default values
+AAbnormalManager::AAbnormalManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+AAbnormalManager *AAbnormalManager::GetInstance(const UObject *WorldContextObject)
+{
+	static AAbnormalManager *Instance = nullptr;
+	if (!Instance->IsValidLowLevel())
+	{
+		if (auto It = TActorIterator<AAbnormalManager>(WorldContextObject->GetWorld()))
+		{
+			Instance = *It;
+		}
+		else
+		{
+			Instance = WorldContextObject->GetWorld()->SpawnActor<AAbnormalManager>();
+			UE_LOG(LogAbnormalPlugin, Error, TEXT("Dynamic Spawn AbnormalManager, No data is filled in"));
+		}
+	}
+	return Instance;
+}
+
+
+void AAbnormalManager::TriggerTask_Implementation(const FString& AbnormalId, const FString& AbnormalTaskName, const TArray<FTransform>& TargetTransforms)
+{
+	if (auto AbnormalTask = Abnormals.Find(AbnormalTaskName))
+	{
+		if (AbnormalTask)
+		{
+			for (auto &AbnormalInfo : AbnormalTask->AbnormalsInfo)
+			{
+				auto SpawnedTargetActors = SpawnTargetActor(AbnormalId, TargetTransforms);
+				AbnormalInfo.TargetActors.Append(SpawnedTargetActors);
+				if (AbnormalInfo.bDynamicGenerateActor)
+				{
+					if (auto Class = AbnormalInfo.AbnormalClass.Get())
+					{
+						SpawnAndBindingToTargetActor(AbnormalId, Class, AbnormalInfo.TargetActors);
+					}
+				}
+				else
+				{
+				}
+			}
+		}
+	}
+}
+
+void AAbnormalManager::SpawnAndBindingToTargetActor_Implementation(const FString& AbnormalId, UClass* AbnormalClass, const TArray<class AActor*>& TargetActors)
+{
+	auto AttachRule = FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
+	for (const auto& TargetActor : TargetActors)
+	{
+		if(auto AbnormalActor = GetWorld()->SpawnActor<AAbnormalBase>(AbnormalClass, FVector::ZeroVector, FRotator::ZeroRotator, FActorSpawnParameters()))
+		{
+			AbnormalActor->Tags.Emplace(FName(*AbnormalId));
+			AbnormalActor->AttachToActor(TargetActor, AttachRule);
+			// TODO 换成委托，通知已经绑定结束，在播放动画前留一个余地做额外的准备操作
+			AbnormalActor->StartPlaySequence();
+		}
+	}
+}
+
+const TArray<class AActor*> AAbnormalManager::SpawnTargetActor_Implementation(const FString& AbnormalId, const TArray<FTransform>& TargetTransforms)
+{
+	TArray<class AActor*> TargetActors;
+	for (const auto& TargetTransform : TargetTransforms)
+	{
+		if(auto TargetActor = GetWorld()->SpawnActor<ATargetPoint>(ATargetPoint::StaticClass(), TargetTransform, FActorSpawnParameters()))
+		{
+			TargetActor->Tags.Emplace(FName(*AbnormalId));
+			TargetActors.Emplace(TargetActor);
+		}
+	}
+	return TargetActors;
+}
+
+
+bool AAbnormalManager::DestroyAbnormalActorsById_Implementation(const FString& AbnormalId)
+{
+	TArray<AActor*> ActorsWithAbnormalIdTag;
+	UGameplayStatics::GetAllActorsWithTag(this, FName(*AbnormalId), OUT ActorsWithAbnormalIdTag);
+	bool bAllDestroy = true;
+	for (auto& Actor : ActorsWithAbnormalIdTag)
+	{
+		if (Actor)
+		{
+			bAllDestroy &= Actor->Destroy();
+		}
+	}
+	return bAllDestroy;
+}
+
+// Called when the game starts or when spawned
+void AAbnormalManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAbnormalManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Uncategorized/Plugins/AbnormalPlugin/Source/AbnormalPlugin/Public/AbnormalManager.h
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AbnormalBase.h"
+#include "AbnormalManager.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAbnormalInfo
+{
+	GENERATED_USTRUCT_BODY()
+	/// 动态生成非正常Actor标识，从实例中选择和从类中选择只能二选一
+	UPROPERTY(EditAnywhere, Category = Abnormal, meta = (InlineEditConditionToggle))
+	bool bDynamicGenerateActor = true;
+	/// 场景中已存在的非正常Actor，具体类型的非正常，如铁路维修人员施工非正常Actor、石头滚落Actor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal, meta = (EditCondition = "!bDynamicGenerateActor"))
+	class AAbnormalBase* AbnormalActor;
+	/// 未拖入场景中的非正常Actor类，只能指定AbnormalBase类及其子类
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal, meta = (EditCondition = "bDynamicGenerateActor"))
+	TSubclassOf<AAbnormalBase> AbnormalClass;
+	/// 欲挂载的目标点，一个单位的非正常动画可以挂载到多个目标点上
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal)
+	TArray<AActor*> TargetActors;
+
+	/// 默认播放一次，设置为0或负数表示无限循环播放
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal, AdvancedDisplay)
+	int32 PlayCount = 1;
+	/// 设置延迟播放的时间
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal, AdvancedDisplay)
+	float DelaySeconds;
+};
+
+USTRUCT(BlueprintType)
+struct FAbnormalsInfo
+{
+	GENERATED_USTRUCT_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal)	
+	TArray<FAbnormalInfo> AbnormalsInfo;
+};
+
+UCLASS()
+class ABNORMALPLUGIN_API AAbnormalManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAbnormalManager();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abnormal)
+	TMap<FString, FAbnormalsInfo> Abnormals;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Abnormal, meta = (WorldContext = "WorldContextObject", DisplayName = "Get Abnormal Manager"))
+	static AAbnormalManager* GetInstance(const UObject* WorldContextObject);
+	
+	/**
+	 * @brief 开始执行任务
+	 * 
+	 * @param AbnormalId 任务ID
+	 * @param AbnormalTaskName 任务名，用来和Abnormals中的键对应
+	 * @param TargetTransforms 位置信息，用来生成TargetPoint
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Abnormal)
+	void TriggerTask(const FString& AbnormalId, const FString& AbnormalTaskName, const TArray<FTransform>& TargetTransforms);
+
+	void TriggerTask_Implementation(const FString& AbnormalId, const FString& AbnormalTaskName, const TArray<FTransform>& TargetTransforms);
+
+	/**
+	 * @brief 根据TargetActors的个数生成AbnormalClass指定类的Actor
+	 * 生成的Actor依次附加到TargetActor上
+	 * 
+	 * @param AbnormalId 任务ID，生成Actor时添加Tag，用于删除Actor时索引 
+	 * @param AbnormalClass 要生成Actor的类
+	 * @param TargetActors 要附加的Actor数组
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Abnormal)
+	void SpawnAndBindingToTargetActor(const FString& AbnormalId, UClass* AbnormalClass, const TArray<class AActor*>& TargetActors);
+
+	void SpawnAndBindingToTargetActor_Implementation(const FString& AbnormalId, UClass* AbnormalClass, const TArray<class AActor*>& TargetActors);
+
+	/**
+	 * @brief 根据TargetTransforms在每一个Transform生成一个ATargetPoint
+	 * 并为每一个Actor设置值为AbnormalId的Tag
+	 * 
+	 * @param AbnormalId 
+	 * @param TargetTransforms 
+	 * @return const TArray<class AActor*> 生成的Actor数组
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Abnormal)
+	const TArray<class AActor*> SpawnTargetActor(const FString& AbnormalId, const TArray<FTransform>& TargetTransforms);
+
+	const TArray<class AActor*> SpawnTargetActor_Implementation(const FString& AbnormalId, const TArray<FTransform>& TargetTransforms);
+
+	/**
+	 * @brief 根据IdTag销毁动态生成的TargetActor和AbnormalActor
+	 * 
+	 * @param AbnormalId 
+	 * @return true 如果每一个带有AbnormalId标签的Actor销毁成功则返回
+	 * @return false 只要有一个没有销毁成功返回false
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Abnormal)
+	bool DestroyAbnormalActorsById(const FString& AbnormalId);
+
+	bool DestroyAbnormalActorsById_Implementation(const FString& AbnormalId);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
